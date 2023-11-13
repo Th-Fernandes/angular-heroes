1cd34154b667b30a1f52159836fbee00fae9c1de
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-11-13 18:52:46 -0300

    feat: hero details path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { SelectedHeroComponent } from './selected-hero/selected-hero.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent, title: 'Dashboard - AngularHeroes' },
-  { path: 'heroes', component: HeroesComponent, title: 'All Heroes - AngularHeroes' }
+  { path: 'heroes', component: HeroesComponent, title: 'All Heroes - AngularHeroes' },
+  { path: 'hero/detail/:id', component: SelectedHeroComponent }
 ]
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from 'src/interfaces/hero';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor( private heroService: HeroService ) {}
+  heroes?: Hero[]
 
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
+  }
 }
