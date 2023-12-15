98c647ad29ca191a4a21c140c8d73ab92bf6954c
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-12-15 11:34:52 -0300

    refactor: remove unused methods, duplicated propeties and create semantic new methods

--- src/app/core/services/user-search.service.ts
+++ src/app/core/services/user-search.service.ts
@@ -1,29 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Hero } from 'src/interfaces/hero';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserSearchService {
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
-  searchEntry = this.formBuilder.control('');
-  searchEntry2 = new Subject<Hero[]>();
-
-  whenSearchEntryChanges(): Observable<string | null> {
-    return this.searchEntry.valueChanges;
-  }
+  constructor(private http: HttpClient) {}
+  searchEntry = new Subject<Hero[]>();
 
   registerUserInput() {
     this.getJobs().subscribe(res => {
-      this.searchEntry2.next(res)
+      this.searchEntry.next(res)
     })
   }
   
   getJobs() {
     return this.http.get<Hero[]>('api/heroes')
   }
-
 }
--- src/app/feature/jobs-page/jobs-page.component.ts
+++ src/app/feature/jobs-page/jobs-page.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { UserSearchService } from 'src/app/core/services/user-search.service';
 import { Hero } from 'src/interfaces/hero';
 
@@ -9,25 +7,15 @@ import { Hero } from 'src/interfaces/hero';
   templateUrl: './jobs-page.component.html',
 })
 export class JobsPageComponent implements OnInit {
-  constructor(
-    private userSearch: UserSearchService,
-    private http: HttpClient
-  ) {}
+  constructor(private userSearch: UserSearchService) {}
 
-  jobs: Hero[] = []
-  httpOptions = new HttpHeaders({ 'Content-Type': 'application/json' })
+  jobs: Hero[] = [];
 
   ngOnInit() {
-    this.userSearch.searchEntry2.subscribe(values => this.jobs = values)
-    // this.getAvailableJobs().subscribe(res => this.jobs.push(...res))
-
-    // this.userSearch.whenSearchEntryChanges().subscribe(searchEntry => {
-
-    //   console.log('using method ', searchEntry)
-    // })
+    this.getJobsByUserSearchEntry();
   }
 
-  getAvailableJobs():Observable<Hero[]> {
-    return this.http.get<Hero[]>('api/heroes')
+  getJobsByUserSearchEntry() {
+    this.userSearch.searchEntry.subscribe((values) => (this.jobs = values));
   }
 }
